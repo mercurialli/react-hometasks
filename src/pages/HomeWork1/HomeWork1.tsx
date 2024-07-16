7de413f3aec2a4130e063565f6ee1d24988d3dd6
mercurialli <ninoklala2@gmail.com>
2024-07-16 09:07:20 +0300

    add dice

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,28 @@
+import style from "./hw.module.css";
+import { useState } from "react";
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+  const [dicesSide, setDicesSide] = useState<number>(1);
+  const [onAnimation, setOnAnimation] = useState<boolean>(false);
+  const sides = [1, 2, 3, 4, 5, 6];
+  const handleRollDice = () => {
+    const rollDice = () => Math.floor(Math.random() * sides.length) + 1;
+    setOnAnimation(true);
+    setDicesSide(rollDice);
+  };
+  return (
+    <div className={style.diceContainer}>
+      <h1 className={style.title}>Игральный кубик</h1>
+      <div className={onAnimation ? style.diceAnimated : style.dice}>
+        {dicesSide}
+      </div>
+      <button
+        className={style.button}
+        onMouseDown={handleRollDice}
+        onClick={() => setOnAnimation(false)}
+      >
+        Бросить кубик
+      </button>
+    </div>
+  );
 };
